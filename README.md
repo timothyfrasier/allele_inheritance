1635767bbc93e26f62808bb7f98475bbd3df7376
Tim Frasier <timothy.frasier@smu.ca>
2026-07-10 12:20:20 -0300

    Initial commit of allele inheritance files

---
+++ README.md
@@ -0,0 +1,108 @@
+# INSTRUCTIONS FOR USING THE ALLELE INHERITENCE R SCRIPTS
+
+These scripts are an R adaptation of the C code described in Frasier (2008), which is implemented in the STORM program for testing hypotheses regarding allele inheritance. Specifically, they allow users to test the hypothesis that offspring inherit paternal alleles differing from the maternal alleles more/less often than expected given the parents genotypes and Mendelian inheritance.
+
+There are two versions of the scripts: one that reads entire data files into R, and another that only reads part of the data files into R at a time. There are two example data sets, each containing three files: a file containing offspring genotypes, a file containing the parental genotypes, and a file containing all genotypes combined for use in calculating allele frequencies.
+
+These instructions assume that the files are laid out as follows:
+    - The data files are in a "data" directory,
+    - The scripts are in a "code" directory, and
+    - You have set R's working directory to the
+      "code" directory
+
+|-| code/
+|   |- allele-inheritance_all-in.R
+|   |- allele-inheritance_partial.R
+|
+|-| data/
+|   |- genotypes_full.csv
+|   |- genotypes_5000.csv
+|   |- offspring_5000.csv
+|   |- offspring_full.csv
+|   |- parents_5000.csv
+|   |- parents_full.csv
+
+Instructions and notes are also present within the code itself that may be helpful as a guide.
+
+------
+
+## File Descriptions:     
+
+    1. allele-inheritance_all_in.R
+        - Contains functions for conducting analyses reading in the entire data files into R.
+
+    2. allele-inheritance_partial.R
+        - Contains functions for conducting analyses reading in just part of the file(s) at a time.
+
+    3. offspring_5000.csv
+        - Data for 3 offspring genotyped at 5000 SNPs
+
+    4. offspring_full.csv
+        - Data for 3 offspring genotyped at 57,704 SNPs
+
+    5. parents_5000.csv
+        - Genotypes for the 3 parental pairs at 5000 SNPs
+        - Parents pairs must be in the same order as the offspring. For example, rows 1 and 2 must be the genotypes of the parents of the offspring in row 1 of the offspring file. Rows 3 and 4 must be the genotypes of the parents of the offspring in row 2 of the offspring file. And so on.
+
+    6. parents_full.csv
+        - Genotypes for the 3 parental pairs at 57,704 SNPs
+        - Parents pairs must be in the same order as the offspring. For example, rows 1 and 2 must be the genotypes of the parents of the offspring in row 1 of the offspring file. Rows 3 and 4 must be the genotypes of the parents of the offspring in row 2 of the offspring file. And so on.
+
+    7. genotypes_5000.csv
+        - Combined offspring and parental genotypes at 5000 SNPs
+
+    8. genotypes_full.csv
+        - Combined offspring and parental genotypes at 57,704 SNPs
+
+-----
+
+## Running analyses reading full data files into R (example using the 5000 SNP data set, but can also use with larger data set).
+
+1. Open RStudio and make the "code" directory R's working directory
+
+2. Load the script into R, so that the functions are available to you
+`source("allele-inheritance_all-in.R")`
+
+3. Make sure that you have the `data.table` package installed
+
+3. Use the `frequencies` function to calculate the allele frequencies. This will write the results to a file called `freqs.csv` in R's working directory. Requires two arguments:
+    a. The name and relative path to the genotypes file ("file"). File *must* be comma-delimited
+    b. The number of loci ("nLoci")
+`frequencies(file = "../data/genotypes_5000.csv", nLoci = 5000)`
+
+4. Use the `ai` function ("ai" for allele inheritance, *not* artificial intelligence) to calculate observed allele inheritance. This will write the results (a single column with one value for each triad) into a file called `observed_ai.csv` within R's working directory. Requires five arguments:
+    a. The name and relative path to the parental genotype file ("pfile"). File *must* be comma-delimited
+    b. The name and relative path to the offspring genotype file ("ofile"). File *must* be comma-delimited
+    c. The name and relative path to the allele frequency file ("ffile")
+    d. The number of loci ("nLoci")
+    e. The number of mother-father-triads ("nTriads")
+`ai(pfile = "../data/parents_5000.csv", ofile = "../data/offspring_5000.csv", ffile = "freqs.csv", nLoci = 5000, nTriads = 3)`
+
+5. Use the `sim` function to generate simulated offspring from each parental pair. **This will take a while to run!** Progress updates will be written to your screen so that you can keep track of progress. But you will need to be patient and have other things to do in the meantime (or perhaps go to sleep and check it in the morning). The output will be a file called `sim_ai.csv` that contains *one value* for each iteration (the average ai across *all* simulated  offspring for that iteration). The result will be a distribution of expected allele inheritance values given just Mendelian inheritance. Requires five arguments:
+    a. The name and relative path to the parental genotype file ("pfile"). File *must* be comma-delimited
+    b. The name and relative path to the allele frequency file ("ffile")
+    c. The number of loci ("nLoci")
+    d. The number of triads ("nTriads")
+    e. The number of iterations that you want to conduct (i.e., how many simulated offspring do you want to generate for each parental pair?). I just use 50 here, but to get a good distribution of "expected" values, you should run more (e.g., 1000).
+  
+`sim(pfile = "../data/parents_5000.csv", ffile = "freqs.csv", nLoci = 5000, nTriads = 3, iterations = 50)`
+
+6. You can then compare and visualize the difference between observed and expected values using standard R functions. One example is to plot a histogram of the expected values, and then a red dashed line where the observed value is. Example commands for this are below. *Note that the calculation is how often a paternal allele is inherited that is different from the maternal allele, so observed values larger than expected mean that homozygotes are missing from your data set, and vice versa.* 
+`observed <- read.table("observed_ai.csv", header = FALSE, sep = ",")`
+`expected <- read.table("sim_ai.csv", header = FALSE, sep = ",")`
+`library(ggplot2)`
+`ggplot(expected) +`
+`   theme_bw() +`
+`   geom_histogram(aes(x = V1), alpha = 0.6) +`
+`   geom_vline(xintercept = mean(observed$V1), color = "red", linewidth = 1.5, linetype = "dashed") +`
+`   xlab("Allele Inheritance") +`
+`   ylab("Frequency")
+-----
+
+## Running analyses reading only a portion of the data files into R at a time (example using the 5000 SNP data set, but can also use with larger data set).
+
+The commands for this are the same as above, except for which script you read in.
+`source("allele-inheritance_partial.R")
+
+## References
+Frasier TR (2008) STORM: Software for testing hypotheses of relatedness and mating patterns. *Molecular Ecology Resources* **8**: 1264-1266.
---
+++ code/allele-inheritance_partial.R
@@ -0,0 +1,451 @@
+#####################################################
+#             allele-inheritance.R                  #
+#                                                   #
+# Code for testing hypotheses regarding the rate at #
+# which offspring inherit maternal and paternal     #
+# alleles that are different from each other.       #
+#---------------------------------------------------#
+# It contains 3 main functions that users can       #
+# employ:                                           #
+#                                                   #
+#   1. frequencies: For calculating allele          #
+#      frequencies within a genotype file.          #
+#                                                   #
+#   2. ai: For calculating allele inheritance       #
+#      values for observed offspring.               #
+#                                                   #
+#   3. sim: For simulating offspring from parental  #
+#      genotypes and calculating their ai.          #
+#---------------------------------------------------#
+# It also contains 2 "helper" functions, that the   #
+# user does not interact with directly, but rather  #
+# are called upon by other functions. These are:    #
+#                                                   #
+#   4. mendel: Simulates offspring from known       #
+#      parents (used in the sim function).          #
+#                                                   #
+#   5. aisim: Calculates allele inheritance for the #
+#      simulated offspring (used in the sim         #
+#      function). Code is the same as in the ai     #
+#      function, but some input and output options  #
+#      are set to deal with the simulated data.     #
+#                                                   #
+#                                                   #
+#                        by                         #
+#                    Tim Frasier                    #
+#              Last Updated: 10-JUL-2026            #
+#####################################################
+
+
+######################################################
+#               1. frequencies                       #
+#                                                    #
+# This function calculates the allele frequencies    #
+# (for biallelic loci) within a data set. It         #
+# assumes two columns for each locus and one row     #
+# per individual. The first allele should start      #
+# in column 2 (the first column should have          #
+# sample/individual labels). One allele should be    #
+# labeled as 1, and another as 2. Missing data       #
+# should be indicated as 0.                          #
+#----------------------------------------------------#
+# This function requires two pieces of input:        #
+#                                                    #
+#   1. file: The name of the file containing the     #
+#      genotypes (including the relative path to     #
+#      it from the working directory); and           #
+#                                                    #
+#   2. nLoci: The number of loci used.               #
+#                                                    #
+# example:                                           #
+# frequencies(file = "genotypes.txt", nLoci = 10000) #
+######################################################
+
+frequencies <- function(file, nLoci) {
+  
+#-------------------------------#
+#    Load Necessary Packages    #
+#-------------------------------#
+library(data.table)
+
+#-------------------------------#
+#   Get Allele Frequencies      #
+#-------------------------------#
+
+#--- Table for holding results ---#
+freqs <- data.frame(matrix(NA, nrow = 2, ncol = nLoci))
+
+# Loop through loci
+for (i in 1:nLoci) {
+  
+  #--- Print progress to screen (every 500th step) ---#
+  if (i %% 500 == 0) {
+    print(paste("Calculating allele frequencies for locus: ", i))
+  }
+  
+  #--- Get appropriate columns ---#
+  temp <- fread(file, header = FALSE, sep = ",", select = c(i * 2, (i * 2) + 1))
+
+  # Count the number of times each allele appears
+  one <- sum(temp == 1)
+  two <- sum(temp == 2)
+
+  # Write Frequencies
+  freqs[1, i] <- one / (one + two)
+  freqs[2, i] <- two / (one + two)
+}
+
+write.table(freqs, "freqs.csv", sep = ",", quote = FALSE, row.names = FALSE, col.names = FALSE)
+print("Done! Allele frequencies written to file freqs.csv in working directory.")
+}
+
+
+############################################################
+#                        2. ai                             #
+#                                                          #
+# This function calculates the allele inheritance for each #
+# offspring based on whether or not they inherited a       #
+# paternal allele different from that inherited from their #
+# mother. It only does this for informative loci, and      #
+# weights these values based on the expected               #
+# heterozygosity at those same loci.                       #
+#----------------------------------------------------------#
+# It requires 5 pieces of information from the user:       #
+#                                                          #
+#   1. pfile: The name (and path to) the file containing   #
+#      the parental genotypes.                             #
+#                                                          #
+#   2. ofile: The name (and path to) the file containing   #
+#      the offspring genotypes.                            #
+#                                                          #
+#   3. ffile: The name (and path to) the file containing   #
+#      the allele frequencies (generated using the         #
+#      frequencies function of this package.               #
+#                                                          #
+#   4. nLoci: The number of loci used.                     #
+#                                                          #
+#   5. nTriads: The number of triads being analyzed.       #
+#                                                          #
+# example:                                                 #
+# ai(pfile = "parents.csv", ofile = "offspring.csv",       #
+#    ffile = "freqs.csv", nLoci = 5000, nTriads = 3)       #
+############################################################
+
+ai <- function(pfile, ofile, ffile, nLoci, nTriads) {
+
+#-------------------------------#
+#    Load Necessary Packages    #
+#-------------------------------#
+library(data.table)
+  
+# Vector for holding ai values for each offspring
+allele_inheritance <- rep(NA, times = nTriads)
+
+for (i in 1:nTriads) {
+  
+  # Vector to hold Expected Heterozygosity
+  het <- rep(NA, times = nLoci)
+  
+  # Vector to hold the number of typed loci
+  typed <- rep(NA, times = nLoci)
+  
+  # Vector to hold the number of informative loci
+  iLoci <- rep(NA, times = nLoci)
+  
+  # Vector to hold if inherited alleles differ
+  differ <- rep(NA, times = nLoci)
+  
+  for (j in 1:nLoci) {
+    
+    #--- Print progress to screen (every 500th step) ---#
+    if (j %% 500 == 0) {
+      print(paste("Processing triad ", i, "locus ", j))
+    }
+    
+    #---------------------------------------------------------#
+    # Get appropriate parental, offspring, and frequency data #
+    #---------------------------------------------------------#
+    parents1 <- fread(pfile, header = FALSE, sep = ",", select = c(j * 2, (j * 2) + 1))
+    parents <- parents1[((i * 2) - 1):(i * 2), ]
+
+    offspring1 <- fread(ofile, header = FALSE, sep = ",", select = c(j * 2, (j * 2) + 1))
+    offspring <- offspring1[i, ]
+
+    freqs <- fread(ffile, header = FALSE, sep = ",", select = c(j))
+
+    
+    #-----------------------------------#
+    # Calculate Expected Heterozygosity #
+    #-----------------------------------#
+    if ((sum(parents == 0) + sum(offspring == 0)) > 0) {
+      het[j] <- 0
+      typed[j] <- 0
+    } else {
+      het[j] <- 1 - (as.numeric(freqs[1, 1])^2 + as.numeric(freqs[2, 1])^2)
+      typed[j] <- 1
+    }
+    
+    
+    #---------------------------------#
+    #  Is Locus Informative?          #
+    #---------------------------------#
+    if (typed[j] == 0) {
+      iLoci[j] <- 0
+    } else {
+      if (sum(parents) == 5) {
+        iLoci[j] <- 1
+      } else {
+        if ((sum(parents) == 6) && (as.numeric(parents[1, 1]) != as.numeric(parents[1, 2]))) {
+          iLoci[j] <- 1
+        } else {
+          if (sum(parents) == 7) {
+            iLoci[j] <- 1
+          } else {
+            iLoci[j] <- 0
+          }
+        }
+      }
+    }
+  
+    #-----------------------------------#
+    #     Do Alleles Differ?            #
+    #-----------------------------------#
+    if (typed[j] == 0) {
+      differ[j] <- 0
+    } else {
+      if (iLoci[j] == 0) {
+        differ[j] <- 0
+      } else {
+        if(as.numeric(offspring[1, 1]) == as.numeric(offspring[1, 2])) {
+        differ[j] <- 0
+      } else {
+        differ[j] <- 1
+      }
+      }
+    }
+  }
+  allele_inheritance[i] <- (sum(differ) / sum(iLoci)) / (sum(het) / sum(typed))
+}
+write.table(allele_inheritance, "observed_ai.csv", sep = ",", quote = FALSE, row.names = FALSE, col.names = FALSE)
+print("Done! Results written to file observed_ai.csv.")
+}
+
+############################################################
+#                        3. sim                            #
+#                                                          #
+# This function runs two other functions (mendel and aisim)#
+# to simulate offspring from each mating pair by randomly  #
+# selecting one allele from each parent, and then          #
+# calculates the average allele inheritance across the     #
+# simulated data set. Thus, the output is one value of     #
+# allele inheritance (the mean for all offspring) for each #
+# iteration.                                               #
+#----------------------------------------------------------#
+# It requires 5 pieces of information from the user:       #
+#                                                          #
+#   1. pfile: The name (and path to) the file containing   #
+#      the parental genotypes.                             #
+#                                                          #
+#   2. ffile: The name (and path to) the file containing   #
+#      the allele frequencies (generated using the         #
+#      frequencies function of this package.               #
+#                                                          #
+#   3. nLoci: The number of loci used.                     #
+#                                                          #
+#   4. nTriads: The number of triads being analyzed.       #
+#                                                          #
+#   5. iterations: The number of iterations to conduct     #
+#      (i.e., the number of simulated data sets to create  #
+#      and then analyse).                                  #
+#                                                          #
+############################################################
+
+sim <- function(pfile, ffile, nLoci, nTriads, iterations) {
+ 
+  #-------------------------------#
+  #    Load Necessary Packages    #
+  #-------------------------------#
+  library(data.table)
+  
+  # Vector for holding results
+  sim_ai <- rep(NA, times = iterations)
+  
+  for (n in 1:iterations) {
+    
+    # Generate simulated offspring
+    mendel(pfile = pfile, nTriads = nTriads, nLoci = nLoci, n = n)
+    
+    # Calculated their allele-inheritance
+    sim_ai[n] <- aisim(pfile = pfile, ffile = ffile, nLoci = nLoci, nTriads = nTriads, n = n)
+  }
+  write.table(sim_ai, "sim_ai.csv", sep = ",", quote = FALSE, row.names = FALSE, col.names = FALSE)
+  print("Done! Results written to file sim_ai.csv.")
+}  
+  
+  
+#----------------------------------------------------------------------------#
+#                             HELPER FUNCTIONS                               #
+#                                                                            #
+# The functions below are not used directly by the user. Instead, they are   #
+# called upon by other functions.                                            #
+#----------------------------------------------------------------------------#
+
+############################################
+#                 mendel                   #
+#                                          #
+# This function takes parental genotypes   #
+# and generates simulated offspring by     #
+# randomly selecting one allele from each  #
+# parent. It then calculates the allele-   #
+# inheritance of those offspring and       #
+# returns the average ai for those         #
+# simulated offspring.                     #
+#------------------------------------------#
+############################################
+
+mendel <- function(pfile, nTriads, nLoci, n) {
+
+# Create file for holding simulated offspring
+file.create("simOff.csv")
+
+for (i in 1:nTriads) {
+  
+  # Vector to hold results
+  offspring <- rep(NA, times = ((2 * nLoci) + 1))
+  
+  # Number simulated offspring
+  offspring[1] <- i
+  
+  for (j in 1:nLoci) {
+    
+    #--- Print progress to screen (every 500th step) ---#
+    if (j %% 500 == 0) {
+      print(paste("Iteration", n, "Simulating offspring", i, "locus", j))
+    }
+    
+    #-------------------------------#
+    # Get appropriate parental data #
+    #-------------------------------#
+    parents1 <- fread(pfile, header = FALSE, sep = ",", select = c(j * 2, (j * 2) + 1))
+    parents <- parents1[((i * 2) - 1):(i * 2), ]
+    
+    #-----------------------#
+    #  Generate Offspring   #
+    #-----------------------#
+    if (sum(parents == 0) > 0) {
+      offspring[j * 2] <- 0
+      offspring[(j * 2) + 1] <- 0
+    } else {
+      offspring[(j * 2)] <- sample(as.numeric(parents[1, ]), size = 1, replace = FALSE)
+      offspring[(j * 2) + 1] <- sample(as.numeric(parents[2, ]), size = 1, replace = FALSE)
+    }
+  }
+  write.table(matrix(offspring, nrow = 1), file = "simOff.csv", append = TRUE, sep = ",", 
+              eol = "\n", row.names = FALSE, col.names = FALSE)
+}
+}
+
+
+############################################################
+#                        aisim                             #
+#                                                          #
+# This is just slighly different than the original ai      #
+# function, designed for easier automation for the         #
+# analysis of simulated offspring.                         #
+############################################################
+
+aisim <- function(pfile, ffile, nLoci, nTriads, n) {
+  
+  # Vector for holding ai values for each offspring
+  allele_inheritance <- rep(NA, times = nTriads)
+  
+  # Specify offspring file
+  ofile <- "simOff.csv"
+  
+  for (i in 1:nTriads) {
+    
+    # Vector to hold Expected Heterozygosity
+    het <- rep(NA, times = nLoci)
+    
+    # Vector to hold the number of typed loci
+    typed <- rep(NA, times = nLoci)
+    
+    # Vector to hold the number of informative loci
+    iLoci <- rep(NA, times = nLoci)
+    
+    # Vector to hold if inherited alleles differ
+    differ <- rep(NA, times = nLoci)
+    
+    for (j in 1:nLoci) {
+      
+      #--- Print progress to screen (every 500th step) ---#
+      if (j %% 500 == 0) {
+        print(paste("Iteration", n, "Calculating ai for triad", i, "locus", j))
+      }
+      
+      #---------------------------------------------------------#
+      # Get appropriate parental, offspring, and frequency data #
+      #---------------------------------------------------------#
+      parents1 <- fread(pfile, header = FALSE, sep = ",", select = c(j * 2, (j * 2) + 1))
+      parents <- parents1[((i * 2) - 1):(i * 2), ]
+      
+      offspring1 <- fread(ofile, header = FALSE, sep = ",", select = c(j * 2, (j * 2) + 1))
+      offspring <- offspring1[i, ]
+      
+      freqs <- fread(ffile, header = FALSE, sep = ",", select = c(j))
+      
+      
+      #-----------------------------------#
+      # Calculate Expected Heterozygosity #
+      #-----------------------------------#
+      if ((sum(parents == 0) + sum(offspring == 0)) > 0) {
+        het[j] <- 0
+        typed[j] <- 0
+      } else {
+        het[j] <- 1 - (as.numeric(freqs[1, 1])^2 + as.numeric(freqs[2, 1])^2)
+        typed[j] <- 1
+      }
+      
+      
+      #---------------------------------#
+      #  Is Locus Informative?          #
+      #---------------------------------#
+      if (typed[j] == 0) {
+        iLoci[j] <- 0
+      } else {
+        if (sum(parents) == 5) {
+          iLoci[j] <- 1
+        } else {
+          if ((sum(parents) == 6) && (as.numeric(parents[1, 1]) != as.numeric(parents[1, 2]))) {
+            iLoci[j] <- 1
+          } else {
+            if (sum(parents) == 7) {
+              iLoci[j] <- 1
+            } else {
+              iLoci[j] <- 0
+            }
+          }
+        }
+      }
+      
+      #-----------------------------------#
+      #     Do Alleles Differ?            #
+      #-----------------------------------#
+      if (typed[j] == 0) {
+        differ[j] <- 0
+      } else {
+        if (iLoci[j] == 0) {
+          differ[j] <- 0
+        } else {
+          if(as.numeric(offspring[1, 1]) == as.numeric(offspring[1, 2])) {
+            differ[j] <- 0
+          } else {
+            differ[j] <- 1
+          }
+        }
+      }
+    }
+    allele_inheritance[i] <- (sum(differ) / sum(iLoci)) / (sum(het) / sum(typed))
+  }
+  return(mean(allele_inheritance))
+}
